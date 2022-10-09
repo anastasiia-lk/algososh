88de90f9f2c348ae94243c3bc36ce504eace5fa9
Anastasiia Duplishcheva <a.duplishcheva@gmail.com>
2022-10-09 22:51:19 +0300

    add string input

--- algososh/src/components/string/string.tsx
+++ algososh/src/components/string/string.tsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { ChangeEvent, useCallback, useState } from 'react';
+
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { Button } from '../ui/button/button';
+import { Input } from '../ui/input/input';
+
+import styles from './string.module.css';
 
 export const StringComponent: React.FC = () => {
+  const [inputValue, setInputValue] = useState('');
   return (
     <SolutionLayout title="Строка">
-     
+      <div className={styles['flex-wrapper']}>
+      <Input
+        isLimitText
+        maxLength={11}
+        value={inputValue}
+        // onChange={onChange}
+        />
+      <Button
+        text='Развернуть'
+        linkedList='small'
+        // isLoader={isLoader}
+        // onClick={onClick}
+        // disabled={disabled}
+      />
+    </div>
     </SolutionLayout>
   );
 };
